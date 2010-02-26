7b4e3b24becdb0fd31e08441392915d776aeae47
Elan Ruusamäe <glen@pld-linux.org>
2010-02-26 11:16:38 +0000

    - make class useful, print java.class.version system property
    Changed files:
        Test.java -> 1.2

--- Test.java
+++ Test.java
@@ -3,7 +3,11 @@
  * We test this by compiling class and looking it's version.
  */
 
+import java.util.*;
+
 public class Test {
     public static void main(String[] args) {
+		Properties p = System.getProperties();
+		System.out.println(p.get("java.class.version"));
 	}
 }
